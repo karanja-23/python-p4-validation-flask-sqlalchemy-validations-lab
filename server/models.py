8c3919c83f586ddb8847a83c394d72a6485c481b
Hosea <karanja.mungai23@gmail.com>
2025-01-21 13:08:44 +0300

    passed tests

--- server/models.py
+++ server/models.py
@@ -12,7 +12,19 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
-
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Name is required')
+        
+        for author in Author.query.all():
+            if name == author.name:
+                raise ValueError('Name must be unique')
+        return name
+    @validates('phone_number')
+    def validates_phone_number(self, key, number):
+        if not number.isdigit() or len(number) != 10:
+            raise ValueError('Phone numbers should have 10 digits')
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -28,7 +40,23 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('content')
+    def validates_content(self, key, content):
+        if not isinstance(content,str) or len(content) > 250 :
+            raise ValueError("Post content is at least 250 characters long")
 
-
+    @validates('summary')
+    def validates_summary(self, key, content):
+        if not isinstance(content,str) or len(content) > 250 :
+            raise ValueError("Post content is at least 250 characters long")   
+    @validates('category')
+    def validates_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError(' category is either Fiction or Non-Fiction')  
+@validates('title')
+def validates_title(self, key, title):
+    keywords =  ["Won't Believe", "Secret", "Top", "Guess"]
+    if not isinstance(title, str) or not any(keyword in title for keyword in keywords):
+        raise ValueError('Title must contain one of the following: "Won\'t Believe", "Secret", "Top", "Guess"')
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
